2a0308d451a13c239659e5721150fd0b5e6c4cb1
lununglee <leo.lunung.lee@gmail.com>
2020-03-18 12:36:10 -0700

    added thrust up

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+	Rigidbody	RB_Rocket;
 	// Start is called before the first frame update
 	void	Start()
 	{
-		
+		RB_Rocket = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
@@ -21,6 +22,7 @@ public class Rocket : MonoBehaviour
 		if (Input.GetKey(KeyCode.Space)) /** Thruster */
 		{
 			print("Space is pressed");
+			RB_Rocket.AddRelativeForce(Vector3.up);
 		}
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) /** Tilt left */
 		{
