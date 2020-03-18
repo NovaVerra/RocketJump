08a991643d3a49f86a8526c37928f7fc60a9acd1
lununglee <leo.lunung.lee@gmail.com>
2020-03-18 12:08:11 -0700

    implemented input binding

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void	Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void	Update()
+	{
+		ProcessInput();
+	}
+
+	void	ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space)) /** Thruster */
+		{
+			print("Space is pressed");
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) /** Tilt left */
+		{
+			print("Left Arrow is pressed");
+		}
+		else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) /** Tilt right */
+		{
+			print("Right Arrow is pressed");
+		}
+		else
+		{
+			;
+		}
+	}
+}
