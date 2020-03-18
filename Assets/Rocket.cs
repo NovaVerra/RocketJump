e0778a86023dabc8d8fee4c86351b69f637de31a
lununglee <leo.lunung.lee@gmail.com>
2020-03-18 15:00:32 -0700

    added audio component

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 	Rigidbody	RB_Rocket;
+	AudioSource	A_RocketRumble;
 	// Start is called before the first frame update
 	void	Start()
 	{
 		RB_Rocket = GetComponent<Rigidbody>();
+		A_RocketRumble = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -23,14 +25,22 @@ public class Rocket : MonoBehaviour
 		{
 			print("Space is pressed");
 			RB_Rocket.AddRelativeForce(Vector3.up);
+			if (!A_RocketRumble.isPlaying)
+				A_RocketRumble.Play();
+		}
+		else
+		{
+			A_RocketRumble.Stop();
 		}
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) /** Tilt left */
 		{
 			print("Left Arrow is pressed");
+			transform.Rotate(Vector3.forward);
 		}
 		else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) /** Tilt right */
 		{
 			print("Right Arrow is pressed");
+			transform.Rotate(-Vector3.forward);
 		}
 		else
 		{
