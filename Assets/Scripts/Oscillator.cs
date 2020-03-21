5a41ecfa79dac947f5ca1680fa09d9cf86fb74a9
lununglee <leo.lunung.lee@gmail.com>
2020-03-21 14:11:03 -0700

    manually slide obstacles exposing slider and desired location

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+	/** Game Configuration */
+	[SerializeField]
+	Vector3	MovementVector;
+
+	[Range(0, 1)] [SerializeField] 
+	float	MovementFactor;
+
+	Vector3	StartingPosition;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartingPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3	Offset = MovementVector * MovementFactor;
+		transform.position = StartingPosition + Offset;
+	}
+}
